4b2fda5532c890de01e72f5ae0d655245b1ffb46
GerardoLagunes <pepislander@gmail.com>
2021-08-04 20:38:11 -0600

    asdadq

--- app.js
+++ app.js
@@ -1,1 +1,24 @@
 console.log("Hola tu mamon");
+/*Customer satisfaction API*/
+
+const express = require('express');
+const cors = require('cors');
+
+// Global app object
+const app = express();
+
+// Middleware config
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({
+    extended: true
+}));
+
+app.get("/", function(req, res){
+    res.send("Welcome to costumer satisfaction API")
+});
+
+// Bootstrap server
+const server = app.listen(process.env.PORT || 3000, function () {
+    console.log(`Listening on port ${server.address().port}`);
+});
